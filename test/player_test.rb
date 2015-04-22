b360c67fb86ba83cc5dcbc147d1fb29963e56b5b
Ari Russo <ari.russo@gmail.com>
2015-04-22 09:17:27 -0400

    template for player test

---
+++ test/player_test.rb
@@ -0,0 +1,16 @@
+require "helper"
+
+class MMPlayer::PlayerTest < Minitest::Test
+
+  context "Player" do
+
+    setup do
+      @player = MMPlayer::Player.new
+    end
+
+    context "#progress" do
+
+    end
+
+  end
+end
