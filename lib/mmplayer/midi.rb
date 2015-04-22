fd88664ffc7e7b9e4cd2071824ecaa41e421577a
Ari Russo <ari.russo@gmail.com>
2015-04-21 21:51:08 -0400

    eager initialize

--- lib/mmplayer/midi.rb
+++ lib/mmplayer/midi.rb
@@ -3,43 +3,35 @@ module MMPlayer
   class MIDI
 
     attr_accessor :channel
+    attr_reader :config
 
     def initialize(input)
       @channel = nil
-      @input = input
-    end
-
-    def note(num, &callback)
-      config[:note][num] = callback
-    end
-
-    def cc(num, &callback)
-      config[:cc][num] = callback
-    end
-
-    def start
-      listener.on_message(:channel => @channel, :class => MIDIMessage::NoteOn) do |event|
-        message = event[:message]
-        config[:note][message.note].call(message.note)
-      end
-      listener.on_message(:channel => @channel, :class => MIDIMessage::ControlChange) do |event|
-        message = event[:message]
-        config[:cc][message.note].call(message.value)
-      end
-      listener.start(:background => true)
-    end
-
-    private
-
-    def config
-      @config ||= {
+      @config = {
         :note => {},
         :cc => {}
       }
+      @listener = MIDIEye::Listener.new(input)
     end
 
-    def listener
-      @listener ||= MIDIEye::Listener.new(@input)
+    def note(num, &callback)
+      @config[:note][num] = callback
+    end
+
+    def cc(num, &callback)
+      @config[:cc][num] = callback
+    end
+
+    def start
+      @listener.on_message(:channel => @channel, :class => MIDIMessage::NoteOn) do |event|
+        message = event[:message]
+        @config[:note][message.note].call(message.note)
+      end
+      @listener.on_message(:channel => @channel, :class => MIDIMessage::ControlChange) do |event|
+        message = event[:message]
+        @config[:cc][message.note].call(message.value)
+      end
+      @listener.start(:background => true)
     end
 
   end
