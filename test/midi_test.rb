9e7db81cf672a83e090a9e11cc0e70e09d97d172
Ari Russo <ari.russo@gmail.com>
2015-04-23 23:21:06 -0400

    system message callback test

--- test/midi_test.rb
+++ test/midi_test.rb
@@ -12,7 +12,7 @@ class MMPlayer::MIDITest < Minitest::Test
     context "#start" do
 
       setup do
-        @midi.listener.expects(:on_message).twice
+        @midi.listener.expects(:on_message).times(3)
         @midi.listener.expects(:start).once
       end
 
@@ -41,6 +41,20 @@ class MMPlayer::MIDITest < Minitest::Test
 
     end
 
+    context "#add_system_callback" do
+
+      setup do
+        @var = nil
+        @midi.add_system_callback(:start) { |val| @var = val }
+      end
+
+      should "store callback" do
+        refute_nil @midi.config[:system][:start]
+        assert_equal Proc, @midi.config[:system][:start].class
+      end
+
+    end
+
     context "#add_cc_callback" do
 
       setup do
@@ -51,6 +65,63 @@ class MMPlayer::MIDITest < Minitest::Test
       should "store callback" do
         refute_nil @midi.config[:cc][2]
         assert_equal Proc, @midi.config[:cc][2].class
+      end
+
+    end
+
+    context "#channel=" do
+
+      setup do
+        # stub out MIDIEye
+        @listener = Object.new
+        @listener.stubs(:events).returns([])
+        @midi.instance_variable_set("@listener", @listener)
+      end
+
+      teardown do
+        @listener.unstub(:clear)
+        @listener.unstub(:on_message)
+        @listener.unstub(:events)
+        @listener.unstub(:running?)
+      end
+
+      context "before listener is started" do
+
+        setup do
+          @listener.stubs(:running?).returns(false)
+          @listener.expects(:clear).never
+          @listener.expects(:on_message).never
+        end
+
+        should "change channel" do
+          assert_equal 3, @midi.channel = 3
+          assert_equal 3, @midi.channel
+        end
+
+        should "set channel nil" do
+          assert_equal nil, @midi.channel = nil
+          assert_nil @midi.channel
+        end
+      end
+
+      context "after listener is started" do
+
+        setup do
+          @listener.stubs(:running?).returns(true)
+          @listener.expects(:clear).once
+          @listener.expects(:on_message).times(3)
+        end
+
+        should "change channel" do
+          assert_equal 3, @midi.channel = 3
+          assert_equal 3, @midi.channel
+        end
+
+        should "set channel nil" do
+          assert_equal nil, @midi.channel = nil
+          assert_nil @midi.channel
+        end
+
       end
 
     end
