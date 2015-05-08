2a950c0e6919656e409dba7f4af9e4c74cc91b0a
Ari Russo <ari.russo@gmail.com>
2015-05-07 20:41:24 -0400

    add test for Instructions::Player#on_end_of_file

--- test/instructions/player_test.rb
+++ test/instructions/player_test.rb
@@ -10,6 +10,22 @@ class MMPlayer::Instructions::PlayerTest < Minitest::Test
       assert @context.kind_of?(MMPlayer::Instructions::Player)
     end
 
+    context "#on_end_of_file" do
+
+      setup do
+        @context.player.expects(:add_end_of_file_callback).once.returns({})
+      end
+
+      teardown do
+        @context.player.unstub(:add_end_of_file_callback)
+      end
+
+      should "assign callback" do
+        refute_nil @context.on_end_of_file { something }
+      end
+
+    end
+
     context "#method_missing" do
 
       setup do
